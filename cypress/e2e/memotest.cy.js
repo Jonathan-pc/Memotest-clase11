eb3ed566c53a8e4c7aa7dbb1cefaf30e1079ce8b
Jonathan <jgervassio@gmail.com>
2024-11-03 17:29:29 -0300

    Agrega mas pruebas
    Agrega mas pruebas y corrige sintaxis de varios it

--- cypress/e2e/memotest.cy.js
+++ cypress/e2e/memotest.cy.js
@@ -4,23 +4,30 @@ describe('Memotest Game', () => {
     cy.visit('src/index.html');
   });
 
-  it('El temporizador y los intentos inician en 0', () => {
+  it('el temporizador y los intentos inician en 0', () => {
     cy.get('#temporizador').should('have.text', '0');
     cy.get('#intentos').should('have.text', '0');
   });
 
-  it('El tablero contiene 16 cartas', () => {
+  it('el temporizador comienza a contar después de girar la primera carta', () => {
+    cy.get('#tablero .carta').first().click();
+    cy.wait(2000);
+    cy.get('#temporizador').should('not.have.text', '0');
+  });
+  
+
+  it('el tablero contiene 16 cartas', () => {
     cy.get('#tablero .carta').should('have.length', 16);
   });
 
-  it('Girar dos cartas y verificar intentos', () => {
+  it('girar dos cartas y verificar intentos', () => {
     cy.get('#tablero .carta').first().click();
     cy.get('#tablero .carta').eq(1).click();
 
     cy.get('#intentos').should('have.text', '1');
   });
 
-  it('Emparejar todas las cartas y verificar mensaje de victoria', () => {
+  it('emparejar todas las cartas y verificar mensaje de victoria', () => {
     for (let i = 0; i < 16; i += 2) {
       cy.get('#tablero .carta').eq(i).click();
       cy.get('#tablero .carta').eq(i + 1).click();
@@ -29,4 +36,36 @@ describe('Memotest Game', () => {
       expect(text).to.include('Ganaste!');
     });
   });
+
+  it('las cartas se voltean si no coinciden', () => {
+    cy.get('#tablero .carta').first().click();
+    cy.get('#tablero .carta').eq(1).click();
+    
+    cy.wait(1100);
+    
+    cy.get('#tablero .carta').first().should('not.have.class', 'girada');
+    cy.get('#tablero .carta').eq(1).should('not.have.class', 'girada');
+  });
+  
+  it('no se puede girar la misma carta dos veces seguidas', () => {
+    cy.get('#tablero .carta').first().click();
+    cy.get('#tablero .carta').first().click();
+    cy.get('#intentos').should('have.text', '0');
+  });  
+
+  it('el orden de las cartas es aleatorio en cada partida', () => {
+    let ordenPrimerPartida = [];
+    cy.get('#tablero .carta').each(($el, index) => {
+      ordenPrimerPartida[index] = $el.attr('data-value');
+    }).then(() => {
+      cy.reload();
+      let ordenSegundaPartida = [];
+      cy.get('#tablero .carta').each(($el, index) => {
+        ordenSegundaPartida[index] = $el.attr('data-value');
+      }).then(() => {
+        expect(ordenPrimerPartida).to.not.deep.equal(ordenSegundaPartida);
+      });
+    });
+  });
+  
 });
